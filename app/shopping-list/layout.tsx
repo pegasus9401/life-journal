86c661b40df1365e71d1ed2ab672edb810b72d71
pegasus9401 <pegasus9401@gmail.com>
2026-08-16 15:48:39 +0300

    Add store filtering and repair shopping list theme

--- app/shopping-list/layout.tsx
+++ app/shopping-list/layout.tsx
@@ -1,2 +1,3 @@
 import "../../styles/meal-planning.css";
+import "../../styles/shopping-light.css";
 export default function ShoppingListLayout({ children }: { children: React.ReactNode }) { return children; }
